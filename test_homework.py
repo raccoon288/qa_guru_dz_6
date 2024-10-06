6e183e90e5708e1273038442356e4851d8114c22
Valeria Oshurkova <v.osh.frrr@gmail.com>
2024-10-06 18:18:06 +0300

    Fix -- Добавлена проверка времени в тест test_dark_theme_by_time_and_user_choice

--- test_homework.py
+++ test_homework.py
@@ -25,10 +25,17 @@ def test_dark_theme_by_time_and_user_choice():
     """
     current_time = time(hour=16)
     dark_theme_enabled_by_user = True
-    # TODO переключите темную тему в зависимости от времени суток,
-    #  но учтите что темная тема может быть включена вручную
 
-    is_dark_theme = None
+    if dark_theme_enabled_by_user:
+        is_dark_theme = True
+    elif dark_theme_enabled_by_user == False:
+        is_dark_theme = False
+    elif dark_theme_enabled_by_user is None:
+        if time(hour=22) <= current_time <= time(hour=23) or time(hour=0) <= current_time <= time(hour=6):
+            is_dark_theme = True
+        else:
+            is_dark_theme = False
+
     assert is_dark_theme is True
 
 
